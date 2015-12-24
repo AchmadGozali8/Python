57597d9c5570bf7f545c08e7dfdfbe32a38fe685
AchmadGozali8 <achmadg6@gmail.com>
2015-12-24 21:56:36 +0800

    Push to github

---
+++ ImplementasiOperatorDanLoop.py
@@ -0,0 +1,9 @@
+print "implementasi loop dan operator"
+print ""
+i = 1
+hasil = 0
+while i <= 15:
+	print "loop ke - %d = %d + %d"%(i,hasil,i)
+	hasil = hasil + i
+	i += 1
+print "totalnya adalah= ",hasil
---
+++ operator_perbandingan.py
@@ -0,0 +1,12 @@
+print "operator perbandigan"
+print ""
+a = 15
+b = 15
+c = 1
+d = 76
+
+print a > c #true
+print a < d # true
+print a == d #false
+print a != c #true
+print a <> d #true
